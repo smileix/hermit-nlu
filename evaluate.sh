c508f119dd9c006126fe676e00852592dcfa2320
Smile <53631271+smileix@users.noreply.github.com>
2021-07-05 15:19:28 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-python main.py -n hermit --units 200 --optimizer rmsprop --dropout 0.8 -m testing --run-folder evaluation --train-set "datasets/nlu_benchmark_hrc2/KFold_1/trainset" --test-set "datasets/nlu_benchmark_hrc2/KFold_1/testset" -g 0,1,2,3
-
+for i in {1..10}
+do
+    python main.py -n hermit --units 200 --optimizer rmsprop --dropout 0.8 -m testing --run-folder evaluation --train-set "datasets/nlu_benchmark_hrc2/KFold_$i/trainset" --test-set "datasets/nlu_benchmark_hrc2/KFold_$i/testset" -g 0
+done
